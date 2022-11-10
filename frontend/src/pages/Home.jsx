159023ce8311b6b2da6a19a6adbb6050ab3e318d
Julien GRELLIER <grllr.julian.ptrs@gmail.com>
2022-11-10 10:57:09 +0100

    css home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -83,11 +83,14 @@ export default function Home() {
         </h2>
       </header>
       <main>
-        <div>
-          <input type="text" placeHolder="Pseudo" onChange={nextBtn} />
-          <div>
+        <div className="circle" />
+        <div className="inputs">
+          <div className="txt">
+            <input type="text" placeHolder="Pseudo" onChange={nextBtn} />
+          </div>
+          <div className="Btns">
             {showBtn ? (
-              <div className="buttons">
+              <div className="btnDif">
                 <Button
                   id="1"
                   type="Easy"
@@ -117,7 +120,7 @@ export default function Home() {
               false
             )}
             {selectedDifficulty && showBtn ? (
-              <div className="buttons">
+              <div className="btnGe">
                 <Button
                   id="4"
                   type="Rock"
@@ -141,25 +144,28 @@ export default function Home() {
               false
             )}
           </div>
-          {selectedGenre && selectedDifficulty && showBtn ? (
-            <div>
-              <Link to="/answer">
-                <StartButton id="7" className="startGame" />
-              </Link>
+          <div className="startRules">
+            {selectedGenre && selectedDifficulty && showBtn ? (
+              <div className="BtnStart">
+                <Link to="/answer">
+                  <StartButton id="7" className="startGame" />
+                </Link>
+              </div>
+            ) : (
+              false
+            )}
+            <div className="modale">
+              <button
+                className="modal"
+                type="button"
+                onClick={() => setShowModal(true)}
+              >
+                ?
+              </button>
             </div>
-          ) : (
-            false
-          )}
-          <button
-            className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-            type="button"
-            onClick={() => setShowModal(true)}
-          >
-            Rules
-          </button>
+          </div>
         </div>
       </main>
-      <footer>footer</footer>
     </>
   );
 }
